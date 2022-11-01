90726afd29cb71063771671b3400e9c03ab7ef46
Luciano Sampaio <lsampaioweb@gmail.com>
2022-11-01 00:05:00 -0300

    Added the terraform specific files for the stagging and production folder.

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -1,6 +1,8 @@
 module "proxmox-ubuntu-22-04" {
   source = "../modules/proxmox-ubuntu-22-04"
 
-  clone = "ubuntu-22-04-server-raw"
-  name  = "ubuntu-22-04-server-raw-vm-production"
+  clone       = "ubuntu-22-04-desktop-raw"
+  name        = "ubuntu-22-04-desktop-raw-vm-production"
+  description = "Ubuntu 22.04 desktop VM with bare minimum settings"
+  pool        = "Production"
 }
